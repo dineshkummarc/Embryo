e21af4009a446d9c956831debc005346caa67b73
Mark McDonnell <mark.mcdx@gmail.com>
2012-01-15 14:51:58 +0000

    Split CSS into seperate modules and update when.js version

--- Assets/Scripts/Utils/dom.js
+++ Assets/Scripts/Utils/dom.js
@@ -1,7 +1,9 @@
 define(function(require){
 
 	return {
+		elementSiblings: require('Utils/elementSiblings'),
 		insertAdjacentHTML: require('Utils/insertAdjacentHTML'),
+		insertAfter: require('Utils/insertAfter'),
 		outerHTML: require('Utils/outerHTML')
 	};
 
