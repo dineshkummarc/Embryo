c89c10b9b87492c80748bf2155081ee3a4e0f2c0
Mark McDonnell <mark.mcdx@gmail.com>
2012-01-16 16:12:10 +0000

    Add additional ES5 Array methods and remove old files

--- Assets/Scripts/Utils/array.js
+++ Assets/Scripts/Utils/array.js
@@ -4,6 +4,10 @@ define(function(require){
 		filter: require('Utils/Array/filter'),
 		forEach: require('Utils/Array/forEach'),
 		indexOf: require('Utils/Array/indexOf'),
+		lastIndexOf: require('Utils/Array/lastIndexOf'),
+		map: require('Utils/Array/map'),
+		reduce: require('Utils/Array/reduce'),
+		reduceRight: require('Utils/Array/reduceRight'),
 		unique: require('Utils/Array/unique')
 	};
 
