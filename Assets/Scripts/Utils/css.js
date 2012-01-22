0286bdc93e43b6dd998b66df8793b97224c097a1
Mark McDonnell <mark.mcdx@gmail.com>
2012-01-22 15:20:47 +0000

    Add native host implementation of classList

--- Assets/Scripts/Utils/css.js
+++ Assets/Scripts/Utils/css.js
@@ -1,11 +1,15 @@
-define(['require'], function(require){
+define(['Utils/CSS/getAppliedStyle', 
+		'Utils/CSS/getArrayOfClassNames', 
+		'Utils/CSS/addClass', 
+		'Utils/CSS/removeClass', 
+		'Utils/CSS/hasClass'], function(getAppliedStyle, getArrayOfClassNames, addClass, removeClass, hasClass){
 
 	return {
-		style: require('Utils/CSS/getAppliedStyle'),
-		classes: require('Utils/CSS/getArrayOfClassNames'),
-		add: require('Utils/CSS/addClass'),
-		remove: require('Utils/CSS/removeClass'),
-		has: require('Utils/CSS/hasClass')
+		style: getAppliedStyle,
+		classes: getArrayOfClassNames,
+		add: addClass,
+		remove: removeClass,
+		has: hasClass
 	}
 
 });
--- Assets/Scripts/twitter.js
+++ Assets/Scripts/twitter.js
@@ -1,4 +1,4 @@
-define(['async!http://twitter.com/statuses/user_timeline/integralist.json'], function(tweets){
+define(['async!http://twitter.com/statuses/user_timeline/integralist.json', 'Utils/css'], function(tweets, css){
 
 	console.log(tweets);
 
