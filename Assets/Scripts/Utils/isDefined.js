1101fc55d4fbd85211a5e875f40f92c48f833737
Mark McDonnell <mark.mcdx@gmail.com>
2012-01-22 15:27:02 +0000

    Add isDefined method

---
+++ Assets/Scripts/Utils/isDefined.js
@@ -0,0 +1,13 @@
+define(function(){
+
+	var isDefined = function(o) {
+		/**
+		 * Item is defined if it's not null or undefined.
+		 * We use strict equality for null as the ES3 specification allows host implementation to return 'null' value as 'object'
+		 */
+		return o !== null && o !== undefined;
+	};
+	
+	return isDefined;
+
+});
