cff4b6c65f02034595cfeb973d6cc9c249972ee4
Mark McDonnell <mark.mcdx@gmail.com>
2012-01-23 14:39:30 +0000

    Clarify comment

--- Assets/Scripts/Utils/isDefined.js
+++ Assets/Scripts/Utils/isDefined.js
@@ -3,7 +3,9 @@ define(function(){
 	var isDefined = function(o) {
 		/**
 		 * Item is defined if it's not null or undefined.
-		 * We use strict equality for null as the ES3 specification allows host implementation to return 'null' value as 'object'
+		 * 
+		 * We use strict equality for null rather than checking the 'typeof' result 
+		 * as the ES3 specification allows host implementation to return 'null' value as 'object'
 		 */
 		return o !== null && o !== undefined;
 	};
