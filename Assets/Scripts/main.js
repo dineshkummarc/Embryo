f2f81bf3ca93387dba767fe3dedd9c51bd462acb
Mark McDonnell <mark.mcdx@gmail.com>
2012-02-08 16:45:10 +0000

    Add RequireJs Error Handling tweaks

--- Assets/Scripts/main.js
+++ Assets/Scripts/main.js
@@ -1,9 +1,17 @@
 require.config({ 
+	catchError: {
+		define: true
+	},
 	paths: {
 		async: 'Plugins/async',
 		jquery: 'Utils/Libraries/jquery',
 		tpl: 'Plugins/tpl'
 	}
+});
+
+require(['errorhandler'], function(handler) {
+	console.log('error handler loaded');
+	require.onError = handler;
 });
 
 require(['videos', 'twitter']);
