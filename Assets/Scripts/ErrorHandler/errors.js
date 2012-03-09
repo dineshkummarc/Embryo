54fa0ddc1abc62cddc292500dab077593aad551e
Mark McDonnell <mark.mcdx@gmail.com>
2012-03-09 08:55:19 +0000

    Error script was only returning undefined so just returned object itself

--- Assets/Scripts/ErrorHandler/errors.js
+++ Assets/Scripts/ErrorHandler/errors.js
@@ -1,9 +1,7 @@
 define(function(){
 	
 	return function(errObject) {
-		requireType = errObject.requireType; 
-        requireModules = errObject.requireModules;
-        console.log(requireType, requireModules);
+		console.log(errObject);
 	};
 	
 });
